417568be85c9f61250a2eac95bb39f7dd93b0e9a
Laomo1982 <mojingyufly@163.com>
2016-01-07 14:14:27 +0800

    Initial Commit

---
+++ VLCController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VLCController
+//
+//  Created by mojingyu on 16/1/7.
+//  Copyright © 2016年 Mojy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VLCControllerTests/VLCControllerTests.m
@@ -0,0 +1,39 @@
+//
+//  VLCControllerTests.m
+//  VLCControllerTests
+//
+//  Created by mojingyu on 16/1/7.
+//  Copyright © 2016年 Mojy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VLCControllerTests : XCTestCase
+
+@end
+
+@implementation VLCControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
